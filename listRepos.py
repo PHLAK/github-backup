d3b56f4668de497be471ce6b7d14a13519e2ae6a
Charles V Bock <Charles@CharlesBock.com>
2013-03-27 14:37:11 -0700

    Update listRepos.py
    PEP8, exception printing, Docstrings, Top level script, Etc.
    
    Pro as shit 4 rlz.

--- listRepos.py
+++ listRepos.py
@@ -1,25 +1,44 @@
 #!/usr/bin/env python
+import sys
+import urllib2
+import json
 
-import sys,urllib2,json
+DEBUG=False
 
-for user in sys.argv:
+def do_debug(debugString):
+    if DEBUG:
+        print (debugString)
+        
+def fetch_repo_list(user):
+    """
+    Fetch user repositories from GitHub.
+    """
+
+    try:
+        endpoint = "https://api.github.com/users/%s/repos" % user
+        apiResults = urllib2.urlopen(endpoint)
+        jsonData = apiResults.read()
+        parse_json(jsonData)
+    except Exception as e:
+        do_debug("Error in Repo Fetch: %s" % e)
 	
-	if user != sys.argv[0]:
-		
-		try:
-			
-			## Fetch user repositories from GitHub
-			apiResults = urllib2.urlopen('https://api.github.com/users/' + user + '/repos')
-			jsonData = apiResults.read()
-			
-			## Parse the json object
-			data = json.loads(jsonData)
-			
-			for repo in data:
+def parse_json(jsonData):	
+    """
+    Parse and print the incomming json.
+    """
+   
+    try:
+        data = json.loads(jsonData)
+        for repo in data:
+            print ( "%s;%s;%s" % (repo['owner']['login'], repo['name'], repo["git_url"]))
+    except Exception as e:
+        do_debug("Error in Json Parse: %s" % e)
+
+def main():
+    for user in sys.argv[1:]:
+        fetch_repo_list(user)
+
+if __name__ == "__main__":
+	main()
+
 				
-				## Print repo info
-				print user + ";" + repo['name'] + ";" + repo["git_url"]
-					
-		except:
-			
-			continue
