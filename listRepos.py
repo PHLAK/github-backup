015e4ae82cf595bd4a294974f43b81b97e3ef35a
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-03-28 07:54:38 -0700

    Modified the way the listRepos.py for loop handled the arguments

--- listRepos.py
+++ listRepos.py
@@ -2,24 +2,22 @@
 
 import sys,urllib2,json
 
-for user in sys.argv:
-	
-	if user != sys.argv[0]:
+for user in sys.argv[1:]:
+			
+	try:
 		
-		try:
+		## Fetch user repositories from GitHub
+		apiResults = urllib2.urlopen('https://api.github.com/users/' + user + '/repos')
+		jsonData = apiResults.read()
+		
+		## Parse the json object
+		data = json.loads(jsonData)
+		
+		for repo in data:
 			
-			## Fetch user repositories from GitHub
-			apiResults = urllib2.urlopen('https://api.github.com/users/' + user + '/repos')
-			jsonData = apiResults.read()
-			
-			## Parse the json object
-			data = json.loads(jsonData)
-			
-			for repo in data:
+			## Print repo info
+			print user + ";" + repo['name'] + ";" + repo["git_url"]
 				
-				## Print repo info
-				print user + ";" + repo['name'] + ";" + repo["git_url"]
-					
-		except:
-			
-			continue
+	except:
+		
+		continue
