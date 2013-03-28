27eb6e35d3b7d32eb1d606f71ba903dba4277b82
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-03-28 07:47:31 -0700

    Reverted listRepos.py for readability

--- listRepos.py
+++ listRepos.py
@@ -1,44 +1,25 @@
 #!/usr/bin/env python
-import sys
-import urllib2
-import json
 
-DEBUG=False
+import sys,urllib2,json
 
-def do_debug(debugString):
-    if DEBUG:
-        print (debugString)
-        
-def fetch_repo_list(user):
-    """
-    Fetch user repositories from GitHub.
-    """
-
-    try:
-        endpoint = "https://api.github.com/users/%s/repos" % user
-        apiResults = urllib2.urlopen(endpoint)
-        jsonData = apiResults.read()
-        parse_json(jsonData)
-    except Exception as e:
-        do_debug("Error in Repo Fetch: %s" % e)
+for user in sys.argv:
 	
-def parse_json(jsonData):	
-    """
-    Parse and print the incomming json.
-    """
-   
-    try:
-        data = json.loads(jsonData)
-        for repo in data:
-            print ( "%s;%s;%s" % (repo['owner']['login'], repo['name'], repo["git_url"]))
-    except Exception as e:
-        do_debug("Error in Json Parse: %s" % e)
-
-def main():
-    for user in sys.argv[1:]:
-        fetch_repo_list(user)
-
-if __name__ == "__main__":
-	main()
-
+	if user != sys.argv[0]:
+		
+		try:
+			
+			## Fetch user repositories from GitHub
+			apiResults = urllib2.urlopen('https://api.github.com/users/' + user + '/repos')
+			jsonData = apiResults.read()
+			
+			## Parse the json object
+			data = json.loads(jsonData)
+			
+			for repo in data:
 				
+				## Print repo info
+				print user + ";" + repo['name'] + ";" + repo["git_url"]
+					
+		except:
+			
+			continue
