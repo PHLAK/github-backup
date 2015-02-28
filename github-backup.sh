5e788151949cbf718262b9208b207dc80efac8b4
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2015-02-27 20:04:51 -0700

    Script cleanup / optimizations

--- github-backup.sh
+++ github-backup.sh
@@ -1,41 +1,41 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ -z $@ ]; then
+if [[ -z "$@" ]]; then
     echo "ERROR: No users specified"
 fi
 
 ## Set vars
-script_path=$(pwd)
-now=$(date +%Y%m%d-%H%M%S)
+script_path="$(pwd)"
+now="$(date +%Y%m%d-%H%M%S)"
 
 ## Get list of repositories
-for line in $(python listRepos.py $@); do
+for line in $(python listRepos.py "$@"); do
 
     ## Set some variables
-    user=$(echo $line | awk -F ';' '{ print $1 }')
-    name=$(echo $line | awk -F ';' '{ print $2 }')
-    repo=$(echo $line | awk -F ';' '{ print $3 }')
+    user=$(echo ${line} | awk -F ';' '{ print $1 }')
+    name=$(echo ${line} | awk -F ';' '{ print $2 }')
+    repo=$(echo ${line} | awk -F ';' '{ print $3 }')
 
     ## Set the bundle path
     bundle_path="${script_path}/bundles/${user}/${now}"
 
     ## Create bundle path if not present
-    if [ ! -d $bundle_path ]; then
-        mkdir -pv $bundle_path
+    if [[ ! -d "${bundle_path}" ]]; then
+        mkdir -pv ${bundle_path}
     fi
 
     ## Set the temp path
     repo_path="/tmp/repos/${user}/${name}"
 
     ## Remove repo if it already exists
-    if [ -d $repo_path ]; then
-        rm -rfv $repo_path
+    if [[ -d "${repo_path}" ]]; then
+        rm -rfv ${repo_path}
     fi
 
     ## Clone the repo and cd into it
-    git clone --mirror $repo $repo_path && cd $repo_path
+    git clone --mirror ${repo} ${repo_path} && cd ${repo_path}
 
     ## Create git bundle
-    git bundle create $bundle_path/$name.bundle --all
+    git bundle create ${bundle_path}/${name}.bundle --all
 
 done
